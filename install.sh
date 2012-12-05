b16a0746722fcdd35cd66a7a4bdcdc3ae797d2d9
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-12-05 11:57:28 -0200

    Adds run_commant to bin

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ for i in `find $(pwd)/dotfiles/ -maxdepth 1`; do
   original_file=`basename $i`
   ln -snfv $i ~/.${original_file}
 done
+
+for i in `find $(pwd)/bin/ -maxdepth 1`; do
+  original_file=`basename $i`
+  ln -snfv $i /usr/local/bin/${original_file}
+done
