0f695b45894446a164ce8bb7dc9d0304ff151fb5
Josemar Luedke <josemarluedke@gmail.com>
2018-07-10 10:42:22 -0700

    Oni config changes

--- config/oni.tsx
+++ config/oni.tsx
@@ -1,39 +1,48 @@
 // For more information on customizing Oni, check out our wiki page:
 // https://github.com/extr0py/oni/wiki/Configuration
 
-import * as React from "/Applications/Oni.app/Contents/Resources/app/node_modules/react"
-import * as Oni from "/Applications/Oni.app/Contents/Resources/app/node_modules/oni-api"
+import * as React from '/Applications/Oni.app/Contents/Resources/app/node_modules/react';
+import * as Oni from '/Applications/Oni.app/Contents/Resources/app/node_modules/oni-api';
 
 export const activate = (oni: Oni.Plugin.Api) => {
+  const isNormalMode = () => oni.editors.activeEditor.mode === 'normal';
+
   oni.input.unbind('<tab>');
-}
+  // oni.input.bind(',m', 'explorer.toggle');
+  // oni.input.bind('t', 'quickOpen.show', isNormalMode);
+};
 
 export const deactivate = (oni: Oni.Plugin.Api) => {
-  console.log("config deactivated")
-}
+  console.log('config deactivated');
+};
 
 export const configuration = {
+  'experimental.indentLines.enabled': true,
   'experimental.markdownPreview.enabled': false,
-  'experimental.edittextMateHighlighting.enabled': false,
+  'editor.textMateHighlighting.enabled': true,
+  'editor.renderer': 'webgl',
+  'explorer.autoRefresh': false,
 
-  'ui.colorscheme': 'base16-nord',
+  'ui.colorscheme': 'gruvbox_dark',
   'autoClosingPairs.enabled': false,
   'oni.useDefaultConfig': false,
   'editor.fontSize': '14px',
   'editor.fontFamily': 'Menlo',
   'editor.linePadding': 4,
+  "editor.quickOpen.defaultOpenMode": 4,
   'tabs.mode': 'tabs',
   'tabs.showIndex': true,
   'oni.exclude': [
+    '**/.git/**',
     '**/node_modules/**',
     '**/dist/**',
     '**/tmp/**',
     '**/vendor/**'
   ],
 
-  "environment.additionalPaths": [
-    "/usr/bin",
-    "/usr/local/bin",
-    "/Users/josemarluedke/code/go/bin"
+  'environment.additionalPaths': [
+    '/usr/bin',
+    '/usr/local/bin',
+    '/Users/josemarluedke/code/go/bin'
   ]
-}
+};
